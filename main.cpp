4b113fd67cbada63a9fbe8844ebbab5a484a3583
Koulik001 <164864836+Koulik001@users.noreply.github.com>
2025-08-15 12:12:15 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "bits/stdc++.h"
 #include "BFSSolver.h"
+#include "DFSSolver.h"
 #include "header.h"
 #include "RubiksCube.cpp"
 #include "RubiksCube3dArray.cpp"
 
 int main(){
     RubiksCube3dArray obj3d;
-    vector<RubiksCube::MOVE> shuffle_moves =  obj3d.randomShuffle(3);
+    vector<RubiksCube::MOVE> shuffle_moves =  obj3d.randomShuffle(6);
     for(auto s: shuffle_moves){
         cout<<obj3d.getMove(s)<<" ";
     }
@@ -15,12 +16,35 @@ int main(){
     
     if(obj3d.isSolved()) cout<<"YES SOLVED....\n";
     else cout<<"NOT SOLVED\n";
+/*-------------------BFS SOLVER------------------ */
+    // BFSSolver<RubiksCube3dArray, Hash3d> cubesolver(obj3d);
+    // vector<RubiksCube::MOVE> solved_moves = cubesolver.solve();
+    // for(auto a: solved_moves){
+    //     cout<<obj3d.getMove(a)<<" ";
+    // }
+    // cout<<"\n";
+    // cubesolver.rubicube.print();
 
-    BFSSolver<RubiksCube3dArray, Hash3d> cubesolver(obj3d);
-    vector<RubiksCube::MOVE> solved_moves = cubesolver.solve();
-    for(auto a: solved_moves){
+/*-------------------DFS SOLVER------------------ */
+    DFSSolver<RubiksCube3dArray, Hash3d> dfs_cubesolver(obj3d, 8);
+    vector<RubiksCube::MOVE> solved_moves_dfs = dfs_cubesolver.solve();
+    for(auto a: solved_moves_dfs){
         cout<<obj3d.getMove(a)<<" ";
     }
     cout<<"\n";
-    cubesolver.rubicube.print();
+    dfs_cubesolver.rubicube.print();
+
+/*-------------- Basic Testing-------------*/
+    // obj3d.move(RubiksCube:: MOVE:: L);
+    // obj3d.move(RubiksCube:: MOVE:: U2);
+    // obj3d.move(RubiksCube:: MOVE:: D2);
+
+    // obj3d.print();
+    // cout<<"\n";
+    // obj3d.move(RubiksCube:: MOVE:: D2);
+    // obj3d.move(RubiksCube:: MOVE:: U);
+    // obj3d.move(RubiksCube:: MOVE:: U);
+    // obj3d.move(RubiksCube:: MOVE:: LPRIME);
+    // obj3d.print();
+    // cout<<"\n";
 }
